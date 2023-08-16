54df69a2075e461506ce02f984a9714ff6c11df0
Abhishek Lal <abhisheklalsxs@gmail.com>
2023-08-16 12:26:44 +0530

    Clean code to remove warnings

--- SpaceCraftTest.java
+++ SpaceCraftTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,14 +10,14 @@ class SpaceCraftTest {
     @Test
     void shouldReturnFinalPositionAsZero() {
         SpaceCraft SC = new SpaceCraft();
-        ArrayList<Integer> expectedList = new ArrayList<Integer>(Arrays.asList(0, 0, 0));
+        ArrayList<Integer> expectedList = new ArrayList<>(Arrays.asList(0, 0, 0));
         assertEquals(expectedList, SC.finalPositionOfSpaceCraft());
     }
 
     @Test
     void shouldReturnFinalPositionAsSetByParameterizedConstructor() {
         SpaceCraft SC = new SpaceCraft(1, 0, 2, "S");
-        ArrayList<Integer> expectedList = new ArrayList<Integer>(Arrays.asList(1, 0, 2));
+        ArrayList<Integer> expectedList = new ArrayList<>(Arrays.asList(1, 0, 2));
         assertEquals(expectedList, SC.finalPositionOfSpaceCraft());
     }
 
